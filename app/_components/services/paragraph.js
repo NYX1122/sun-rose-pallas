bd387c82fd907c42ddaf8bd0a2d6cb24a32b0e6a
BoDee Angus <bodee.angus@gmail.com>
2023-08-11 20:25:22 -0600

    finished mobile version of services page

---
+++ app/_components/services/paragraph.js
@@ -0,0 +1,84 @@
+'use client';
+
+import { useRef, useEffect, useState } from 'react';
+
+export default function Paragraph({
+  shortDescription,
+  description,
+  graphicFirst,
+}) {
+  const childRef = useRef(null);
+  const parentRef = useRef(null);
+  const [parentHeight, setParentHeight] = useState(0);
+
+  useEffect(() => {
+    const calculateHeight = () => {
+      if (childRef.current && parentRef.current) {
+        const childHeight = childRef.current.offsetHeight;
+
+        const parentStyle = window.getComputedStyle(parentRef.current);
+        const parentPaddingTop = parseFloat(parentStyle.paddingTop) || 0;
+        const parentPaddingBottom = parseFloat(parentStyle.paddingBottom) || 0;
+        const parentGap = parseFloat(parentStyle.gap) || 0;
+
+        const totalHeight =
+          childHeight + parentPaddingTop + parentPaddingBottom + parentGap;
+
+        setParentHeight(`${totalHeight}px`);
+      }
+    };
+
+    const timeoutId = setTimeout(calculateHeight, 100); // Or try a longer delay if needed
+
+    return () => clearTimeout(timeoutId);
+  }, [shortDescription]);
+
+  useEffect(() => {
+    function handleResize() {
+      if (childRef.current && parentRef.current) {
+        const childHeight = childRef.current.offsetHeight;
+
+        const parentStyle = window.getComputedStyle(parentRef.current);
+        const parentPaddingTop = parseFloat(parentStyle.paddingTop) || 0;
+        const parentPaddingBottom = parseFloat(parentStyle.paddingBottom) || 0;
+        const parentGap = parseFloat(parentStyle.gap) / 2 || 0;
+
+        const totalHeight =
+          childHeight + parentPaddingTop + parentPaddingBottom + parentGap;
+
+        setParentHeight(`${totalHeight}px`);
+      }
+    }
+
+    window.addEventListener('resize', handleResize);
+
+    // Don't forget to remove the listener when the component unmounts
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  return (
+    <div
+      style={{ maxHeight: parentHeight }}
+      className={`overflow-y-auto border-4 border-white rounded-2xl p-2 bg-[#00000020] flex flex-col gap-2`}
+      ref={parentRef}
+    >
+      <div className='flex flex-col gap-2 max-h-full overflow-y-auto pr-2'>
+        <p
+          ref={childRef}
+          className={`text-xs text-white ${
+            graphicFirst ? 'text-left' : 'text-right'
+          }`}
+        >
+          {shortDescription}
+        </p>
+        <p
+          className={`text-xs text-white ${
+            graphicFirst ? 'text-left' : 'text-right'
+          }`}
+        >
+          {description}
+        </p>
+      </div>
+    </div>
+  );
+}
--- app/information/services/page.js
+++ app/information/services/page.js
@@ -1,5 +1,84 @@
 import Image from 'next/image';
 
+import Paragraph from '@/components/services/paragraph';
+
+import rose from '@/images/Rose.svg';
+import crystal from '@/images/Crystal.svg';
+import card from '@/images/Card.svg';
+import bowl from '@/images/Bowl.svg';
+
 export default function Page() {
-  return <div className='flex flex-col'></div>;
+  const serviceArray = [
+    {
+      title: 'Chakra Balancing & Cleansing',
+      graphic: rose,
+      graphicAlt: 'Graphic of a rose.',
+      shortDescription:
+        'In order for you to connect with your highest self, special attention will be given for balancing, cleansing, and raising each of your chakras to their highest vibration. Your energy, emotionality, and spirituality will be enriched, inspiring peace of mind, and health of body.',
+      description:
+        'We will focus around balancing, cleansing, and raising each of your chakras back to their highest vibration. I will intuitively go through each of your chakras- feeling the energy and determining what kind of influence the flow needs so you can connect with your highest self. I will hold space for you to connect intuitively, whether that is: energetically, emotionally, physically, or spiritually. I will intuitively work with your energy to enhance anything you want to manifest or release.',
+      graphicFirst: true,
+    },
+    {
+      title: 'Crystal Infused Healing',
+      graphic: crystal,
+      graphicAlt: 'Graphic of a crystal.',
+      shortDescription:
+        'The sacred energy within gemstones enhances chakral influence. My assortment of healing crystals permit a bond to the collective universe. An enduring  connection between your internal mind and your eternal soul will grant a lasting sense of self actualization.',
+      description:
+        'We will focus around balancing, cleansing, and raising each of your chakras back to their highest vibration, aided with the energy of crystals. I will intuitively go through each of your chakras- feeling the energy and determining what kind of influence the flow needs so you can connect with your highest self. I will provide crystals, (or you may bring your own), and I will incorporate their energy/influence/magic within your chakras for a complete connection within yourself and the universe.',
+      graphicFirst: false,
+    },
+    {
+      title: 'Card Readings',
+      graphic: card,
+      graphicAlt: 'Graphic of a tarot card.',
+      shortDescription:
+        'Card Readings provide guidance through connecting to your highest self, and the universal energy around you. Allowing the reading to illuminate, educate,  and conceptualize the lessons of the past, the challenges of the present, and the prospects of the future.',
+      description:
+        'I will energetically hold space for you to connect with yourself, your highest-self, as well as the infinite universe around you for any guidance you might be seeking; whether that is knowingly, or not. I will intuitively work with the cards to assist you in receiving any guidance and hopefully providing a clearer sense of direction in life and with self. I will provide the card decks for the session, but you are always more than welcome to provide your own deck if you are feeling drawn to it.',
+      graphicFirst: true,
+    },
+    {
+      title: 'Sound Therapy',
+      graphic: bowl,
+      graphicAlt: 'Graphic of a singing bowl.',
+      shortDescription:
+        'The purity of vibration has a very powerful healing presence, this therapeutic practice increases spiritual awareness while also providing a deep connection to your infinite soul. Guided meditation is also incorporated, allowing the cognizance of universal messages.',
+      description:
+        'I will energetically hold space for you to relax and connect once again with your infinite soul; allowing the strongest sense of healing and awareness to happen. As I intuitively play my singing bowls, each bowl will connect with your different energetic portals (chakras) and allow the opportunity to bring them back to a strong positive flow. Guided meditation is incorporated as well, connecting with your highest self and allowing any universal messages to bring themselves forward. I will also have a drum there to help with breaking away the old, and making space for the new.',
+      graphicFirst: false,
+    },
+  ];
+
+  return (
+    <div className='h-fit flex flex-col gap-6'>
+      {serviceArray.map((item) => (
+        <div className='flex flex-col items-center gap-2'>
+          <h2 className='text-lg text-white'>{item.title}</h2>
+          <div className='flex justify-center gap-2'>
+            {item.graphicFirst ? (
+              <>
+                <Image src={item.graphic} alt={item.graphicAlt} />
+                <Paragraph
+                  shortDescription={item.shortDescription}
+                  description={item.description}
+                  graphicFirst={item.graphicFirst}
+                />
+              </>
+            ) : (
+              <>
+                <Paragraph
+                  shortDescription={item.shortDescription}
+                  description={item.description}
+                  graphicFirst={item.graphicFirst}
+                />
+                <Image src={item.graphic} alt={item.graphicAlt} />
+              </>
+            )}
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 }
