acc3de0619bde825b54280b347c91341fbeaf225
BoDee Angus <bodee.angus@gmail.com>
2023-08-17 20:11:57 -0600

    finally done with location page on mobile

--- app/_components/location/map.js
+++ app/_components/location/map.js
@@ -1,8 +1,75 @@
 'use client';
 
-import { useMemo } from 'react';
-import { GoogleMap, useLoadScript, Marker } from '@react-google-maps/api';
+import { useMemo, useState } from 'react';
+
+import Link from 'next/link';
+
+import {
+  GoogleMap,
+  useLoadScript,
+  Marker,
+  InfoWindow,
+} from '@react-google-maps/api';
 
 export default function Map() {
-  return <div></div>;
+  const [selectedMarker, setSelectedMarker] = useState(null);
+  const markers = [
+    { id: 1, location: { lat: 40.69941889699853, lng: -111.86498463105256 } },
+  ];
+  const center = useMemo(() => markers[0].location, []);
+  const { isLoaded } = useLoadScript({
+    googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
+  });
+
+  if (!isLoaded)
+    return (
+      <div className='w-full h-full border-4 border-white rounded-xl overflow-hidden bg-white'></div>
+    );
+  return (
+    <div className='w-full h-full border-4 border-white rounded-xl overflow-hidden'>
+      <GoogleMap
+        mapTypeId={null}
+        options={{
+          mapId: '9706c4844def0575',
+          mapTypeControl: false,
+          streetViewControl: false,
+        }}
+        zoom={12}
+        center={center}
+        mapContainerStyle={{
+          width: '100%',
+          height: '100%',
+        }}
+      >
+        <Marker
+          position={center}
+          onClick={() => {
+            setSelectedMarker(markers[0]);
+          }}
+        >
+          {selectedMarker && (
+            <InfoWindow
+              position={selectedMarker.location}
+              onCloseClick={() => {
+                setSelectedMarker(null);
+              }}
+            >
+              <div>
+                <h1 className='text-md font-bold'>Sun Rose Pallas Healing</h1>
+                <h1 className='text-md'>3333 South 900 East Street</h1>
+                <h1 className='text-md'>Suite 160</h1>
+                <h1 className='text-md'>Millcreek, Utah</h1>
+                <Link
+                  href='https://goo.gl/maps/atHwhNJngArTmjPV8'
+                  target='_blank'
+                >
+                  <p className='text-md text-blue-600'>View on Google Maps</p>
+                </Link>
+              </div>
+            </InfoWindow>
+          )}
+        </Marker>
+      </GoogleMap>
+    </div>
+  );
 }
--- app/information/location/page.js
+++ app/information/location/page.js
@@ -1,12 +1,42 @@
-import Image from 'next/image';
-
 import Map from '@/components/location/map';
 
 export default function Page() {
   return (
-    <div>
-      <Map />
-      <div></div>
+    <div className='relative border-[.25rem] border-white rounded-xl p-6 bg-center bg-[url("/images/Sunflower_Background.svg")] shadow-xl'>
+      <div className='flex flex-col gap-4'>
+        <div className='border-[.25rem] border-white rounded-xl py-2 bg-peaceful-orange shadow-xl'>
+          <h1 className='text-lg text-white text-center'>
+            3333 South 900 East Street
+          </h1>
+          <div className='flex justify-center items-center'>
+            <div className='h-1 grow bg-white rounded-r-lg'></div>
+            <h1 className='text-lg text-white text-center px-3'>Suite 160</h1>
+            <div className='h-1 grow bg-white rounded-l-lg'></div>
+          </div>
+          <h1 className='text-lg text-white text-center'>Millcreek, Utah</h1>
+        </div>
+        <div className='h-72'>
+          <Map />
+        </div>
+        <div className='border-[.25rem] border-white rounded-xl py-2 bg-peaceful-orange shadow-xl'>
+          <h1 className='text-lg text-white text-center'>
+            For any questions, please call me at
+          </h1>
+          <div className='flex justify-center items-center'>
+            <div className='h-1 grow bg-white rounded-r-lg'></div>
+            <h1 className='text-lg text-white text-center px-3'>
+              (801) 636-4648
+            </h1>
+            <div className='h-1 grow bg-white rounded-l-lg'></div>
+          </div>
+
+          <h1 className='text-lg text-white text-center'>
+            <span style={{ color: 'white' }}>&#9825; </span>
+            Have a wonderful day!
+            <span style={{ color: 'white' }}> &#9825;</span>
+          </h1>
+        </div>
+      </div>
     </div>
   );
 }
